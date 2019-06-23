202e266011f6ef5261fd5f8a696b17751196e332
Takayama Fumihiko <tekezo@pqrs.org>
2019-06-23 21:42:58 +0900

    add applicationShouldTerminateAfterLastWindowClosed

--- nsapplication-example/src/AppDelegate.swift
+++ nsapplication-example/src/AppDelegate.swift
@@ -7,4 +7,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
   func applicationDidFinishLaunching(_: Notification) {}
 
   func applicationWillTerminate(_: Notification) {}
+
+  func applicationShouldTerminateAfterLastWindowClosed(_: NSApplication) -> Bool {
+    return true
+  }
 }

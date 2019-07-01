037ce1d3b9b5ef634fb08bfe79d63deec7d9b6e9
Takayama Fumihiko <tekezo@pqrs.org>
2019-07-02 00:41:51 +0900

    add nsevent-example

--- Makefile
+++ Makefile
@@ -2,12 +2,14 @@ all:
 	make -C cgeventtap-example
 	make -C iokit-hid-value-example
 	make -C nsapplication-example
+	make -C nsevent-example
 	make -C nsview-example
 
 clean:
 	make -C cgeventtap-example clean
 	make -C iokit-hid-value-example clean
 	make -C nsapplication-example clean
+	make -C nsevent-example clean
 	make -C nsview-example clean
 
 dist: all
@@ -16,5 +18,6 @@ dist: all
 	rsync -a cgeventtap-example/build_xcode/build/Release/cgeventtap-example.app dist
 	rsync -a iokit-hid-value-example/build_xcode/build/Release/iokit-hid-value-example.app dist
 	rsync -a nsapplication-example/build_xcode/build/Release/nsapplication-example.app dist
+	rsync -a nsevent-example/build_xcode/build/Release/nsevent-example.app dist
 	rsync -a nsview-example/build_xcode/build/Release/nsview-example.app dist
 	bash ./scripts/codesign.sh dist
---
+++ nsevent-example/src/AppDelegate.swift
@@ -0,0 +1,62 @@
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+  @IBOutlet var window: NSWindow!
+  @IBOutlet private var text: NSTextField!
+  private var timer: Timer?
+  private var eventStrings: [String] = []
+  private var counter: UInt64 = 0
+
+  func applicationDidFinishLaunching(_: Notification) {
+    let options: NSDictionary = [kAXTrustedCheckOptionPrompt.takeRetainedValue() as NSString: true]
+    if !AXIsProcessTrustedWithOptions(options) {
+      timer = Timer.scheduledTimer(
+        withTimeInterval: 3.0,
+        repeats: true
+      ) { _ in self.relaunchIfProcessTrusted() }
+    }
+
+    NSEvent.addGlobalMonitorForEvents(
+      matching: [NSEventMask.keyDown, NSEventMask.keyUp, NSEventMask.flagsChanged],
+      handler: { (event: NSEvent) in
+        switch event.type {
+        case .keyDown:
+          self.updateEventStrings(String(format: "keyDown %d", event.keyCode))
+        case .keyUp:
+          self.updateEventStrings(String(format: "keyUp %d", event.keyCode))
+        case .flagsChanged:
+          self.updateEventStrings(String(format: "flagsChanged %d", event.keyCode))
+        default:
+          break
+        }
+      }
+    )
+  }
+
+  func applicationWillTerminate(_: Notification) {}
+
+  func applicationShouldTerminateAfterLastWindowClosed(_: NSApplication) -> Bool {
+    return true
+  }
+
+  private func relaunchIfProcessTrusted() {
+    if AXIsProcessTrusted() {
+      let task = Process()
+      task.executableURL = URL(fileURLWithPath: Bundle.main.executablePath!)
+      try! task.run()
+      NSApplication.shared().terminate(self)
+    }
+  }
+
+  private func updateEventStrings(_ string: String) {
+    counter += 1
+    eventStrings.append(String(format: "%06d    %@", counter, string))
+
+    while eventStrings.count > 16 {
+      eventStrings.remove(at: 0)
+    }
+
+    text.stringValue = eventStrings.joined(separator: "\n")
+  }
+}

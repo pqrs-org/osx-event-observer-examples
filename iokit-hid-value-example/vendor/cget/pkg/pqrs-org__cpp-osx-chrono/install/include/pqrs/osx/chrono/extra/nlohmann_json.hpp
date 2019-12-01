7462ccb73e4bae507048adfddc7e5c01afbcb299
Takayama Fumihiko <tekezo@pqrs.org>
2019-12-01 17:49:47 +0900

    add pqrs-org/cpp-osx-iokit_hid_value

---
+++ iokit-hid-value-example/vendor/cget/pkg/pqrs-org__cpp-osx-chrono/install/include/pqrs/osx/chrono/extra/nlohmann_json.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+// (C) Copyright Takayama Fumihiko 2019.
+// Distributed under the Boost Software License, Version 1.0.
+// (See http://www.boost.org/LICENSE_1_0.txt)
+
+#include <pqrs/json.hpp>
+#include <pqrs/osx/chrono/absolute_time_point.hpp>
+
+namespace pqrs {
+namespace osx {
+namespace chrono {
+inline void to_json(nlohmann::json& j, const absolute_time_duration& p) {
+  j = type_safe::get(p);
+}
+
+inline void from_json(const nlohmann::json& j, absolute_time_duration& p) {
+  if (!j.is_number()) {
+    using namespace std::string_literals;
+    throw json::unmarshal_error("json must be number, but is `"s + j.dump() + "`"s);
+  }
+
+  p = absolute_time_duration(j.get<int64_t>());
+}
+
+inline void to_json(nlohmann::json& j, const absolute_time_point& p) {
+  j = type_safe::get(p);
+}
+
+inline void from_json(const nlohmann::json& j, absolute_time_point& p) {
+  if (!j.is_number()) {
+    using namespace std::string_literals;
+    throw json::unmarshal_error("json must be number, but is `"s + j.dump() + "`"s);
+  }
+
+  p = absolute_time_point(j.get<uint64_t>());
+}
+} // namespace chrono
+} // namespace osx
+} // namespace pqrs

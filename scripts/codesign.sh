f0f25fa1876c66511338465ab30512e1f49e39b1
Takayama Fumihiko <tekezo@pqrs.org>
2020-04-15 00:50:39 +0900

    update codesign.sh

--- scripts/codesign.sh
+++ scripts/codesign.sh
@@ -1,44 +1,84 @@
 #!/bin/bash
 
-set -e
+set -u # forbid undefined variables
+set -e # forbid command failure
 
-readonly CODESIGN_IDENTITY='8D660191481C98F5C56630847A6C39D95C166F22'
 readonly PATH=/bin:/sbin:/usr/bin:/usr/sbin
 export PATH
+
+readonly CODE_SIGN_IDENTITY=$(bash $(dirname $0)/get-codesign-identity.sh)
+
+if [[ -z $CODE_SIGN_IDENTITY ]]; then
+    echo "Skip codesign"
+    exit 0
+fi
+
+#
+# Define err()
+#
 
 err() {
     echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
 }
 
+#
+# Define main()
+#
+
 main() {
-    if [ ! -e "$1" ]; then
+    if [[ ! -e "$1" ]]; then
         err "Invalid argument: '$1'"
         exit 1
     fi
 
+    #
     # Sign with codesign
+    #
+
     cd "$1"
     find * -name '*.app' -or -path '*/bin/*' | sort -r | while read f; do
+        #
+        # output message
+        #
+
         echo -ne '\033[33;40m'
         echo "code sign $f"
         echo -ne '\033[0m'
 
+        #
+        # codesign
+        #
+
         echo -ne '\033[31;40m'
+
+        set +e # allow command failure
+
         codesign \
             --force \
             --deep \
             --options runtime \
-            --sign "$CODESIGN_IDENTITY" \
-            "$f"
+            --sign "$CODE_SIGN_IDENTITY" \
+            "$f" 2>&1 |
+            grep -v ': replacing existing signature'
+
+        set -e # forbid command failure
+
         echo -ne '\033[0m'
     done
 
+    #
     # Verify codesign
+    #
+
     find * -name '*.app' -or -path '*/bin/*' | sort -r | while read f; do
         echo -ne '\033[31;40m'
         codesign --verify --deep "$f"
         echo -ne '\033[0m'
     done
 }
+
+#
+# Run
+#
 
 main "$1"
